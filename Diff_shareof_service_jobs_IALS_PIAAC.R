8285ed4cd3d0ee421bfef3df706ccdd85cd82a33
Jorge Cimentada <cimentadaj@gmail.com>
2016-08-21 19:11:56 +0200

    Update Diff_shareof_service_jobs_IALS_PIAAC.R

--- Diff_shareof_service_jobs_IALS_PIAAC.R
+++ Diff_shareof_service_jobs_IALS_PIAAC.R
@@ -7,7 +7,7 @@
 
 # and 
 
-# /Users/cimentadaj/Google Drive/Gosta project/PIAAC/surveyanalysis - v2.R
+# /Users/cimentadaj/Google Drive/Gosta project/PIAAC2/Social_mobility_analysis/surveyanalysis - v2.R
 
 library(dplyr)
 c1 <- as.data.frame(cbind(name=vec1,vec2))
